596f5cee0ff30cd8fe0a6afa1ad69299fec877ec
David Lang <langdjii@gmail.com>
2020-06-08 11:42:03 -0500

    update getRecentShopOrders() to convert order to json, modify rest of funcs to accept | add customCheckSubtotalPrice()

--- ShopifyGetsMod.py
+++ ShopifyGetsMod.py
@@ -70,58 +70,54 @@ def getRecentShopOrders(_settings, _print=False):
             financial_status=args['financial_status'],
             limit=args['batch_size'], created_at_min=within_date, since_id=previous_end_id,
         )
+        # Convert orders to json format.
+        for i in range(len(orders)):
+            orders[i] = json.loads(orders[i].to_json().decode('utf-8'))['order']
         # Arguable print for debugging.
         if _print:
             batch_print = "\n>>> in batch {}...  retrieved {} orders...\n>>> order ids:\n{}"
-            print_format = [batch_count, len(orders), ', '.join([ str(o.id) for o in orders ])]
+            print_format = [batch_count, len(orders), ', '.join([ str(o['id']) for o in orders ])]
             print(batch_print.format(*print_format))
         # Build all_orders_, check for while loop break condition, then if loop continues set next
         # loop's previous_end_id.
         all_orders_ += orders
         if len(orders) < args['batch_size']:  break
-        previous_end_id = orders[-1].id
+        previous_end_id = orders[-1]['id']
 
     return all_orders_
 
 
 
-def weFulfillItemsByIdType(_settings, _orders, _id_type, _print=False):
+def weFulfillItemsByIdType(_settings, _orders, _print=False):
     """
     input:  _settings = A Shopify settings module in a json like syntax.  This method requires
                         access to sku_pointers and product_id_pointers to verify if items in _order
                         are items we fulfill.
             _orders =    A list of Shopify Order objects containing all order information.
-            _id_type =  Accepts 'product_id' or 'sku' to designate values of pointers and
-                        pointer_tag.
             _print =    Bool determining whether actions are printed to console (for debug).
     output: Return we_fulfill_ and we_do_not_fulfill_, lists of sorted orders from _orders.  To
             fulfill or not to fulfill determined based on items in orders by _id_type.
     Shopify order reference:  https://shopify.dev/docs/admin-api/rest/reference/orders/order
     """
 
-    # Ensure _id_type is correctly argued.
-    if _id_type not in ['product_id', 'sku']:
-        type_check_exit =  "when calling weFulfillItemsByIdType(), arg _id_type only accepts "
-        type_check_exit += "'product_id' or 'sku', not '{}', please try again"
-        exit(type_check_exit.format(_id_type))
-
     # Get ids_we_fulfill from _settings based on _id_type.
-    ids_we_fulfill = []
-    if _id_type == 'sku':  pointers, pointer_tag = _settings.sku_pointers, 'shop_sku'
-    else:  pointers, pointer_tag = _settings.product_id_pointers, 'shop_product_ids'
-    for pointer in pointers:  ids_we_fulfill += pointer[pointer_tag]
+    product_ids_we_fulfill, skus_we_fulfill = [], []
+    for key, value in _settings.part_pointers.items():
+        if key == 'by_product_id':
+            for pointer in value:  product_ids_we_fulfill += pointer['shop_product_ids']
+        elif key == 'by_sku':
+            for pointer in value:  skus_we_fulfill += pointer['shop_skus']
 
     # BLOCK...  Sort each order in _orders into we_fulfill_ or we_do_not_fulfill_.
     we_fulfill_, we_do_not_fulfill_ = [], []
     for order in _orders:
-        if _print:  print(">>>     checking order {}...  ".format(order.id), end='')
+        if _print:  print(">>>     checking order {}...  ".format(order['id']), end='')
         found_one = False
         # Check each item in line_items to see if it's in ids_we_fulfill.
-        for item in order.line_items:
-            # Set item_id based on _id_type, then see if item_id is in ids_we_fulfill.
-            item_id = item.product_id if _id_type == 'product_id' else item.sku
-            if item_id in ids_we_fulfill:
-                # If so, add order to we_fulfill_, trigger found_one, and end inner for loop.
+        for item in order['line_items']:
+            has_product_id = item['product_id'] in product_ids_we_fulfill
+            has_sku = item['sku'] in skus_we_fulfill
+            if has_product_id or has_sku:
                 we_fulfill_ += [order]
                 found_one = True
                 if _print:  print("has items we fulfill")
@@ -133,14 +129,22 @@ def weFulfillItemsByIdType(_settings, _orders, _id_type, _print=False):
 
     return we_fulfill_, we_do_not_fulfill_
 
-    """   OBSOLETE (2020-05-04)   """
-    # # Return block...  Return True if found item_id in ids_we_fulfill, else Return False.
-    # for item in _order.line_items:
-    #     # Get item_id based on _id_type.
-    #     item_id = item.product_id if _id_type == 'product_id' else item.sku
-    #     if item_id in ids_we_fulfill:  return True
-    # return False
 
+
+def customCheckSubtotalPrice(_key, _order):
+    """
+    input:  _key = Key from shop_settings.shipping_pointers['by_shipping_lines'][''] dict.
+            _order = A Shopify Order object containing all order information.
+    output:  Return (key_, use_default_).  key_ is a modified _key with && flag removed.
+            use_default_ is a bool telling getShipMethod() whether or not to use the default ship
+            method based on value of _order['subtotal_price'].
+    """
+
+    key_, use_default_ = '', True
+    key_ = _key.replace('&check_subtotal_price&', '')
+    if float(_order['subtotal_price']) > 0.00:  use_default_ = False
+
+    return key_, use_default_
 
 
 
@@ -154,24 +158,32 @@ def getShipMethod(_settings, _order):
     """
 
     # Assign ship_method_ value based on default dom/int settings.
-    default_pointers = _settings.shipping_pointers['default']
-    if _order.shipping_address.country == 'United States':  ship_method_ = default_pointers['dom']
-    else:  ship_method_ = default_pointers['int']
+    default_pointers = _settings.shipping_pointers['by_default']
+    if _order['shipping_address']['country'] == 'United States':
+        ship_method_ = default_pointers['dom']
+    else:
+        ship_method_ = default_pointers['int']
 
     # Block...  Assign ship_method_ based on custom shipping_lines settings.
-    if 'shipping_lines' in _settings.shipping_pointers.keys():
-        settings_shipping_lines = _settings.shipping_pointers['shipping_lines']
-        if settings_shipping_lines and _order.shipping_lines:
+    if 'by_shipping_lines' in _settings.shipping_pointers.keys():
+        by_shipping_lines = _settings.shipping_pointers['by_shipping_lines']
+        if _order['shipping_lines']:
             # Different settings can use different types; currently accepting 'code' and 'title'.
-            for type in settings_shipping_lines:
+            for type in by_shipping_lines:
                 # Loop through each pointer set in each type.
-                for key, value in settings_shipping_lines[type].items():
-                    # Loaded as json because Shopify shipping_line object was difficult to parse.
-                    shipping_line = json.loads(_order.shipping_lines[0].to_json().decode('utf-8'))
-                    # Final comparison for ship_method_ assignment.
-                    if shipping_line['shipping_line'][type] == key:
+                for key, value in by_shipping_lines[type].items():
+                    # First if handles (michaelhyatt) custom function.
+                    if key.startswith('&check_subtotal_price&'):
+                        key, use_default = customCheckSubtotalPrice(key, _order)
+                        if use_default:  break
+                    # Final comparison to set ship_method_.
+                    if _order['shipping_lines'][0][type] == key:
                         ship_method_ = value
                         break
+
+    # BLOCK...  Assign ship_method_ based on by_parts pointers.
+    if 'by_parts' in _settings.shipping_pointers.keys():
+        by_parts = _settings.shipping_pointers['by_parts']
 
     return ship_method_
 
@@ -195,19 +207,19 @@ def getShipInfoFromOrder(_settings, _order):
     ship_info_['CompanyID'] =   _settings.credentials['company_id']
     ship_info_['inDate'] =      datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     ship_info_['ReqBy'] =       'Shopify'
-    ship_info_['Company'] =     _order.shipping_address.company
-    ship_info_['Attn'] =        _order.shipping_address.name # need to use string formatting
-    ship_info_['Addy1'] =       _order.shipping_address.address1
-    ship_info_['Addy2'] =       _order.shipping_address.address2
-    ship_info_['City'] =        _order.shipping_address.city
-    ship_info_['State'] =       _order.shipping_address.province
-    ship_info_['Zip'] =         _order.shipping_address.zip
-    ship_info_['Country'] =     _order.shipping_address.country
-    ship_info_['Phone'] =       _order.shipping_address.phone
+    ship_info_['Company'] =     _order['shipping_address']['company']
+    ship_info_['Attn'] =        _order['shipping_address']['name'] # need to use string formatting
+    ship_info_['Addy1'] =       _order['shipping_address']['address1']
+    ship_info_['Addy2'] =       _order['shipping_address']['address2']
+    ship_info_['City'] =        _order['shipping_address']['city']
+    ship_info_['State'] =       _order['shipping_address']['province']
+    ship_info_['Zip'] =         _order['shipping_address']['zip']
+    ship_info_['Country'] =     _order['shipping_address']['country']
+    ship_info_['Phone'] =       _order['shipping_address']['phone']
     ship_info_['ShipMethod'] =  getShipMethod(_settings, _order)
-    ship_info_['ShipNumber'] =  _order.order_number
-    ship_info_['Email'] =       _order.contact_email
-    ship_info_['userdefval2'] = _order.id
+    ship_info_['ShipNumber'] =  _order['order_number']
+    ship_info_['Email'] =       _order['contact_email']
+    ship_info_['userdefval2'] = _order['id']
 
     # Convert None values to empty strings.
     for k, v in ship_info_.items():
@@ -217,7 +229,7 @@ def getShipInfoFromOrder(_settings, _order):
 
 
 
-def getItemsWeFulfillFromOrder(_settings, _order, _id_type):
+def getItemsWeFulfillFromOrder(_settings, _order):
     """
     input:  _settings = A Shopify settings module in a json similar syntax.  This method requires
                         access to sku_pointers an product_id_pointers for shop id to disk id
@@ -229,50 +241,37 @@ def getItemsWeFulfillFromOrder(_settings, _order, _id_type):
     Shopify order reference:  https://shopify.dev/docs/admin-api/rest/reference/orders/order
     """
 
-    # Ensure _id_type is correctly argued.
-    if _id_type not in ['product_id', 'sku']:
-        type_check_exit =  "when calling weFulfill(), arg _id_type only accepts 'product_id' or "
-        type_check_exit += "'sku', not '{}', please try again"
-        exit(type_check_exit.format(_id_type))
-
-    if _id_type == 'sku':  pointers, pointer_tag = _settings.sku_pointers, 'shop_sku'
-    else:  pointers, pointer_tag = _settings.product_id_pointers, 'shop_product_ids'
-
-    # BLOCK...  Build items_to_fulfill_, dict with keys as disk_part_numbers and values as
-    # quantities.
+    # BLOCK...  Build items_to_fulfill_, filter each item in line_items through
+    # _settings.part_pointers.
     items_to_fulfill_ = {}
-    for item in _order.line_items:
-        # Designate item_id based on argued _id_type.
-        item_id = item.product_id if _id_type == 'product_id' else item.sku
-        for pointer in pointers:
-            # The final alignment...  If predesignated item_id is in pointer from predesignated
-            # pointers (referenced with predesignated pointer_tag), then populate items_ with keys
-            # and values from 'disk_part_numbers'.
-            if item_id in pointer[pointer_tag]:
-                for key, value in pointer['disk_part_numbers'].items():
-                    # Note...  Important to multiply qty values from both sources.
-                    items_to_fulfill_[key] = value * item.quantity
+    for item in _order['line_items']:
+        for key, value in _settings.part_pointers.items():
+            # Set item_tag and pointer_tag by key of dict item in part_pointers.
+            if key == 'by_product_id':  item_tag, pointer_tag = 'product_id', 'shop_product_ids'
+            elif key == 'by_sku':  item_tag, pointer_tag = 'sku', 'shop_skus'
+            # Loop through each pointer and compare item[item_tag] with pointer[pointer_tag] to get
+            # disk_part_numbers.
+            for pointer in value:
+                if item[item_tag] in pointer[pointer_tag]:
+                    for key, value in pointer['disk_part_numbers'].items():
+                        items_to_fulfill_[key] = value * item['quantity']
+                        break
 
     return items_to_fulfill_
 
 
 
-def convertOrdersToDiskFormat(_settings, _orders, _items_id_type):
+def convertOrdersToDiskFormat(_settings, _orders):
     """
     input:
     output:
-    """
-
-    """
-    TURNOVER NOTES:
-        - Don't need arg _items_id_type.  That should be extracted from _settings.
     """
 
     disk_orders_ = []
     for order in _orders:
         disk_order = {}
         disk_order['ship_info'] = getShipInfoFromOrder(_settings, order)
-        disk_order['items'] = getItemsWeFulfillFromOrder(_settings, order, _items_id_type)
+        # disk_order['items'] = getItemsWeFulfillFromOrder(_settings, order)
         disk_orders_ += [disk_order]
 
     return disk_orders_
@@ -283,7 +282,85 @@ def convertOrdersToDiskFormat(_settings, _orders, _items_id_type):
                                                                                 ###   OBSOLETE   ###
                                                                                 ####################
 
-"""   OBSOLETE (2020-04-15)   """
+""" OBSOLETE from getShipMethod() (2020-06-08) """
+
+# """ possible bad """
+# # # Loaded as json because Shopify shipping_line object was difficult to parse.
+# # shipping_line = json.loads(_order['shipping_lines'][0].to_json().decode('utf-8'))
+# # # Final comparison for ship_method_ assignment.
+# # if shipping_line['shipping_line'][type] == key:
+# #     ship_method_ = value
+# #     break
+# """ possible good (2020-06-08) """
+# if _order['shipping_lines'][0][type] == key:
+#     ship_method_ = value
+#     break
+
+""" OBSOLETE from weFulfillItemsByIdType() (2020-05-29) """
+# # Ensure _id_type is correctly argued.
+# if _id_type not in ['product_id', 'sku']:
+#     type_check_exit =  "when calling weFulfillItemsByIdType(), arg _id_type only accepts "
+#     type_check_exit += "'product_id' or 'sku', not '{}', please try again"
+#     exit(type_check_exit.format(_id_type))
+#
+# if _id_type == 'sku':  pointers, pointer_tag = _settings.sku_pointers, 'shop_sku'
+# else:  pointers, pointer_tag = _settings.product_id_pointers, 'shop_product_ids'
+# for pointer in pointers:  ids_we_fulfill += pointer[pointer_tag]
+#
+#         # Set item_id based on _id_type, then see if item_id is in ids_we_fulfill.
+#         item_id = item['product_id'] if _id_type == 'product_id' else item['sku']
+#         if item_id in ids_we_fulfill:
+#             # If so, add order to we_fulfill_, trigger found_one, and end inner for loop.
+#             we_fulfill_ += [order]
+#             found_one = True
+
+""" OBSOLETE from getItemsWeFulfillFromOrder() (2020-05-29) """
+# # Ensure _id_type is correctly argued.
+# if _id_type not in ['product_id', 'sku']:
+#     type_check_exit =  "when calling weFulfill(), arg _id_type only accepts 'product_id' or "
+#     type_check_exit += "'sku', not '{}', please try again"
+#     exit(type_check_exit.format(_id_type))
+#
+# if _id_type == 'sku':  pointers, pointer_tag = _settings.sku_pointers, 'shop_sku'
+# else:  pointers, pointer_tag = _settings.product_id_pointers, 'shop_product_ids'
+#
+# # Get ids_we_fulfill from _settings based on _id_type.
+# product_ids_we_fulfill, skus_we_fulfill = [], []
+# for key, value in _settings.part_pointers.items():
+#     if key == 'by_product_id':
+#         for pointer in value:  product_ids_we_fulfill += pointer['shop_product_ids']
+#     elif key == 'by_sku':
+#         for pointer in value:  skus_we_fulfill += pointer['shop_skus']
+#
+# # BLOCK...  Build items_to_fulfill_, dict with keys as disk_part_numbers and values as
+# # quantities.
+# items_to_fulfill_ = {}
+# for item in _order['line_items']:
+#     # Designate item_id based on argued _id_type.
+#     item_id = item['product_id'] if _id_type == 'product_id' else item['sku']
+#     for pointer in pointers:
+#         # The final alignment...  If predesignated item_id is in pointer from predesignated
+#         # pointers (referenced with predesignated pointer_tag), then populate items_ with keys
+#         # and values from 'disk_part_numbers'.
+#         if item_id in pointer[pointer_tag]:
+#             for key, value in pointer['disk_part_numbers'].items():
+#                 # Note...  Important to multiply qty values from both sources.
+#                 items_to_fulfill_[key] = value * item['quantity']
+#
+# for pointer in value:
+#     if item['sku'] in pointer['shop_skus']:
+#         for key, value in pointer['disk_part_numbers'].items():
+#             items_to_fulfill_[key] = value * item['quantity']
+
+""" OBSOLETE from weFulfillItemsByIdType() (2020-05-04) """
+# # Return block...  Return True if found item_id in ids_we_fulfill, else Return False.
+# for item in _order.line_items:
+#     # Get item_id based on _id_type.
+#     item_id = item.product_id if _id_type == 'product_id' else item.sku
+#     if item_id in ids_we_fulfill:  return True
+# return False
+
+""" OBSOLETE (2020-04-15) """
 # def getRecentShopOrders(_within_days=30, _keyargs='default', _batch_size=250, _print=False):
     # """
     # input:  _within_days =  Used to get date stamp of date that is X number days prior to current
